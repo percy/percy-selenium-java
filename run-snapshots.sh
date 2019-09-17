f1aa6049a41e9546c5ed8600ffdaf5a091b3de06
Robert DeLuca <robertdeluca19@gmail.com>
2019-09-17 12:11:43 -0500

    ci: Update version of agent & use yarn
    We handle installing the deps and building the JS outside of the Java build
    pipeline -- there's no reason to duplicate it in Java land too

--- run-snapshots.sh
+++ run-snapshots.sh
@@ -6,8 +6,8 @@ set -e
 # If we don't have a local Percy agent binary, run npm install.
 if [ ! -f ./node_modules/.bin/percy ]; then
   echo "*** Percy agent not installed. Installing now. ***"
-  npm install
+  yarn
 fi
 
 # Run the tests with snapshots.
-./node_modules/.bin/percy exec -- mvn test
+yarn percy exec -- mvn test
