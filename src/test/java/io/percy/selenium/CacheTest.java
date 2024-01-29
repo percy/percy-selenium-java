ea5abaccd23f080e6cb42716ba29c8e4c4d19317
Ninad Sheth <ninad@browserstack.com>
2024-01-29 16:24:20 +0530

    Cleanup

--- src/test/java/io/percy/selenium/CacheTest.java
+++ src/test/java/io/percy/selenium/CacheTest.java
@@ -1,26 +1,13 @@
 package io.percy.selenium;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 //import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.remote.*;
 import static org.mockito.Mockito.*;
@@ -30,8 +17,6 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CacheTest {
     private static RemoteWebDriver mockedDriver;
 
-    private static Percy percy;
-
     @BeforeAll
     public static void testSetup() throws IOException {
         mockedDriver = mock(RemoteWebDriver.class);
@@ -40,7 +25,6 @@ public class CacheTest {
             when(commandExecutor.getAddressOfRemoteServer()).thenReturn(new URL("https://hub-cloud.browserstack.com/wd/hub"));
         } catch (Exception e) {
         }
-        percy = spy(new Percy(mockedDriver));
         when(mockedDriver.getSessionId()).thenReturn(new SessionId("123"));
         when(mockedDriver.getCommandExecutor()).thenReturn(commandExecutor);
         DesiredCapabilities capabilities = new DesiredCapabilities();
